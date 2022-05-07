fc17cbf34854d675ca167d7aab3184efe44f09ce
Pavel Degtyarev <pvldgt@gmail.com>
2022-05-08 00:01:03 +0300

    create the initial labels, entries and buttons without functioning yet

--- main.py
+++ main.py
@@ -18,6 +18,42 @@ canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 # place the image on the canvas
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# website label
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+# email or username label
+username_label = Label(text="Email/Username")
+username_label.grid(column=0, row=2)
+
+# password label
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# website entry
+website_entry = StringVar()
+website = Entry(textvariable=website_entry, width=35)
+website.grid(column=1, row=1, columnspan=2)
+
+# email or username entry
+username_entry = StringVar()
+username = Entry(textvariable=username_entry, width=35)
+username.grid(column=1, row=2, columnspan=2)
+
+# password entry
+password_entry = StringVar()
+password = Entry(textvariable=username_entry, width=21)
+password.grid(column=1, row=3)
+
+# generate password button
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+# add button
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
 
 window.mainloop()
